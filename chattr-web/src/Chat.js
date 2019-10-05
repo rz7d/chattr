a93488108c2980276d34b051f6818b9e5f3a95a1
kash <kashiwa.electory@gmail.com>
2019-10-06 00:48:43 +0900

    refactor

--- chattr-web/src/Chat.js
+++ chattr-web/src/Chat.js
@@ -1,9 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Chat extends React.Component {
-
-    constructor() {
-        super();
+class Chat extends Component {
+    constructor(props) {
+        super(props);
         this.state = { name: "", id: "", content: "" };
     }
 
